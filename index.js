4218238e74d2e44bbd1548d6ed96891477ca322b
Nebgibz <62007358+Nebgibz@users.noreply.github.com>
2020-04-22 22:02:02 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
-});
+    buttonAnimation(buttonInnerHTML);
+  });
 }
 
-document.addEventListener("keydown",function(event){
- makeSound(event.key);
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
-function makeSound(key){
+function makeSound(key) {
   switch (key) {
     case "w":
       var tom1 = new Audio('sounds/tom-1.mp3');
